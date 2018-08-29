c88d49f5c91501f0d1144bc68de9167be72e221f
David Osuna <dosunanet@gmail.com>
2018-08-29 14:28:48 -0700

    Fully Responsive Services Columns

--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -10,6 +10,11 @@ import export_icon from '../../dist/images/export_icon.svg';
 import custom_icon from '../../dist/images/custom_icon.svg';
 import warehouse_icon from '../../dist/images/warehouse_icon.svg';
 import forms_icon from '../../dist/images/forms_icon.svg';
+import tracking_icon from '../../dist/images/tracking_icon.svg';
+import inventory_icon from '../../dist/images/inventory_icon.svg';
+import border_icon from '../../dist/images/border_icon.svg';
+import certified_icon from '../../dist/images/certified_icon.svg';
+import links_icon from '../../dist/images/links_icon.svg';
 
 class Homepage extends React.Component{
 
@@ -61,17 +66,17 @@ class Homepage extends React.Component{
                    <ul className="left-services-column">
                        <li><a href="#import"></a> < img className="importicon" img src={import_icon} /> Import Process </li>
                        <li><a href="#export"></a> <img className="exporticon" img src={export_icon} /> Export Process </li>
-                       <li><a href="#custom"></a> <img className="customicon" img src={custom_icon} /> Custom Consulting </li>
+                       <li><a href="#custom"></a> <img className="customicon" img src={custom_icon} /> Custom Consulting  </li>
                        <li><a href="#warehouse"></a> <img className="warehouseicon" img src={warehouse_icon} /> Warehouse </li>
                        <li><a href="#forms"></a> <img className="formsicon" img src={forms_icon} /> Forms</li>
                    </ul>
 
                    <ul className="right-services-column">
-                       <li><a href="#tracking"></a> Tracking </li>
-                       <li><a href="#inventory"></a> Inventory </li>
-                       <li><a href="#custom"></a> Border Wait Times </li>
-                       <li><a href="#certified"></a> C-TPAD Certified </li>
-                       <li><a href="#links"></a> Links </li>
+                       <li><a href="#tracking"></a> <img className="trackingicon" img src={tracking_icon} /> Tracking </li>
+                       <li><a href="#inventory"></a> <img className="inventoryicon" img src={inventory_icon} /> Inventory </li>
+                       <li><a href="#border"></a> <img className="bordericon" img src={border_icon} /> Border Wait Times </li>
+                       <li><a href="#certified"></a> <img className="certifiedicon" img src={certified_icon} /> C-TPAD Certified </li>
+                       <li><a href="#links"></a> <img className="linksicon" img src={links_icon} /> Links </li>
                    </ul>
 
 
