2ee69aaa358effdf595292e5660fcfa0398d80b0
David Osuna <dosunanet@gmail.com>
2018-09-04 16:17:47 -0500

    Installed React Router for User Page Traversal

---
+++ src/services/ImportPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class ImportPage extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+
+        <h1> Welcome to the Import Page </h1>
+
+    }
+
+}
+
+export default ImportPage;
