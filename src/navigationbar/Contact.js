bb787ef53a46562bb50005103ccfb3df7656801a
David Osuna <dosunanet@gmail.com>
2018-09-05 19:31:03 -0500

    Navigation Bar tasks fulfilled, can iterate again with animations

--- src/navigationbar/Contact.js
+++ src/navigationbar/Contact.js
@@ -1,6 +1,10 @@
 import React from "react";
 import NavBar from "./NavBar";
 import Footer from "../footer/Footer";
+import bsglogo from "../../dist/images/BorderGroupServices.png";
+import email_icon from "../../dist/images/email_icon.svg";
+import usalogo from "../../dist/images/usaflag.png";
+import mexlogo from   "../../dist/images/mexicoflag.png";
 
 import './Contact.css';
 
@@ -19,6 +23,27 @@ class Contact extends React.Component{
                 <NavBar/>
 
                 <div className="contactpage-body">
+
+                    <div className="contactpage-middlecolumn">
+
+                        <h1 className="contactpage_title"> Contact Our Team </h1>
+                        <hr className="contact_hr"/>
+
+                        <img className="bsglogo" img src = {bsglogo} />
+                        <hr className="contact_hr"/>
+
+                        <h2 className="sd_title"> San Diego, California </h2>
+                        <p className="bgs_address"> 9543 Heinrich Hertz Dr # 7, San Diego, CA 92154, USA </p>
+                        <hr className="contact_hr"/>
+
+                        <p className="contact-email"> < img src={email_icon} /> bgsbroker@bgs-chb.com </p>
+                        <hr className="contact_hr"/>
+
+                        <p className="usalogo"> <img src={usalogo} /> (619) 710-2504 </p>
+                        <p className="mexlogo"> <img src={mexlogo} /> (664) 383-3430 </p>
+
+
+                    </div>
 
                 </div>
 
