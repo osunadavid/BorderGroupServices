32a6b8cd447588524a4bec26388404209ad30857
David Osuna <dosunanet@gmail.com>
2018-09-05 21:04:06 -0500

    checking for root problems

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App.js";
-ReactDOM.render(
-
+ReactDOM.render((
     <BrowserRouter>
         <App />
-    </BrowserRouter>,
-    document.getElementById("root")
-);
+    </BrowserRouter>),
+    document.getElementById("root"));
