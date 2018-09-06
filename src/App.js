7b0df4eaeed9a0d24f88f9793f1fa7776610751d
David Osuna <dosunanet@gmail.com>
2018-09-05 21:49:10 -0500

    App is now the main driver

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component} from "react";
 import {hot} from "react-hot-loader";
-import Main from './Main'
+import { Switch, Route } from 'react-router-dom';
+import About from "./navigationbar/About";
+import Contact from "./navigationbar/Contact";
+import Homepage from "./homepage/Homepage";
 
 class App extends Component{
     render(){
         return(
 
                 <div className="app-container">
-                <Main/>
+
+                    <Switch>
+                        <Route exact path='/' component={Homepage}/>
+                        <Route path='/about' component={About} />
+                        <Route path='/contact' component={Contact}/>
+                    </Switch>
+
+
                 </div>
         );
     }
