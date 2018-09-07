4c5a022ce78b71797e58a4cd1c15f47272e6faaf
David Osuna <dosunanet@gmail.com>
2018-09-07 14:30:38 -0500

    New Routing Strategy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component} from "react";
 import {hot} from "react-hot-loader";
-import { Switch, Route } from 'react-router-dom'
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch
+} from 'react-router-dom'
+
 import About from "./navigationbar/About";
 import Contact from "./navigationbar/Contact";
 import Homepage from "./homepage/Homepage";
@@ -8,14 +13,15 @@ import Homepage from "./homepage/Homepage";
 class App extends Component{
     render(){
         return(
-
-            <div className="app-container">
-
-
-              < Homepage/>
-
-
-            </div>
+                <Router basename={process.env.PUBLIC_URL}>
+                    <div className="app-container">
+                    <Switch>
+                        <Route exact path='/' component={Homepage}/>
+                        <Route path='/about' component={About} />
+                        <Route path='/contact' component={Contact}/>
+                    </Switch>
+                    </div>
+                </Router>
         );
     }
 }
