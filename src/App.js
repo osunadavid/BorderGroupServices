939a5075ddc8c273c170a4bf2653bb98eeaded83
David Osuna <dosunanet@gmail.com>
2018-09-13 23:32:05 -0700

    Verify Hosting on GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, { Component} from "react";
 import {hot} from "react-hot-loader";
-import Main from './Main';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch
+} from 'react-router-dom'
+import TransitionSwitch from 'react-router-transition-switch'
+import Contact from "./navigationbar/Contact";
+import ImportPage from "./services/ImportPage";
+import Fader from "react-fader";
+import About from "./navigationbar/About";
+import Homepage from "./homepage/Homepage";
 
 class App extends Component{
     render(){
         return(
                     <div className="app-container">
-                        <Main/>
+                        <Router basename="BorderGroupServices">
+
+                            <TransitionSwitch component={Fader}>
+                                <Route exact path='/' component={Homepage}/>
+                                <Route path='/about' component={About} />
+                                <Route path='/contact' component={Contact}/>
+
+
+                                <Route path='/importpage' component={ImportPage}/>
+
+
+                            </TransitionSwitch>
+
+                        </Router>
                     </div>
         );
     }
